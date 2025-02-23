03d190616e3b8f2e206a68d56046ca578592fec6
fiboscience <you@example.com2696530940@qq.com>
2025-02-23 22:02:43 +0800

    Site updated: 2025-02-23 22:02:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/21/cv-luyuejian/"];function toRandomPost(){
+var posts=["2025/02/21/cv-luyuejian/","2025/02/23/basin/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
