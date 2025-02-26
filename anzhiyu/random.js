9193f8f29c256ee4cc7f43bffddb98611fe91d85
fiboscience <you@example.com2696530940@qq.com>
2025-02-26 22:37:17 +0800

    Site updated: 2025-02-26 22:37:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/23/basin/","2025/02/21/cv-luyuejian/"];function toRandomPost(){
+var posts=["2025/02/23/basin/","2025/02/21/cv-luyuejian/","2025/02/26/imbibition/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
